ebe74dfabf68341c3f71131827a021112f33baa0
Karsten Gresch <git-2015@by-doing.org>
2020-09-04 11:21:12 +0200

    42 example

--- src/main/java/org/acme/people/rest/GreetingResource.java
+++ src/main/java/org/acme/people/rest/GreetingResource.java
@@ -1,21 +1,39 @@
 package org.acme.people.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Path("/hello")
+@Path("/add-answer")
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
 public class GreetingResource {
 
     public static final Logger log = LoggerFactory.getLogger(GreetingResource.class);
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Path("/{inputNumber}")
+    
+    public String addAnswer(@DefaultValue("-42") @PathParam("inputNumber") String inputNumber) {
+        Integer inputNumberConverted = -1;
+        String outputNumber = "-1";
+        
+        if(!inputNumber.isEmpty())
+        {
+            inputNumberConverted = Integer.parseInt(inputNumber);
+            if (inputNumber.isEmpty()) {
+                inputNumberConverted = 0;
+            }
+        }
+        outputNumber = String.valueOf(inputNumberConverted + 42);
+        return outputNumber;
     }
 }
